749aca7cf55fa2f99b27a84cdb52cd889df7827e
Kyo <hyosing92@gmail.com>
2023-03-09 17:19:35 +0900

    feat : NetworkSevice 프로토콜 정의 및 DefaultNetworkSevice 구현

---
+++ K-Market/K-Market/Source/Data/Network/NetworkService.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkService.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/09.
+//
+
+import Foundation
+
+protocol NetworkSevice {
+    func request(_ request: URLRequest?, completion: @escaping (Result<Data, NetworkError>) -> Void)
+}
+
+final class DefaultNetworkSevice: NetworkSevice {
+    func request(_ request: URLRequest?, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        let sesseion = URLSession(configuration: .default)
+        
+        guard let request = request else { return }
+        
+        sesseion.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.networking))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode) else {
+                completion(.failure(.networking))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.data))
+                return
+            }
+            
+            completion(.success(data))
+
+        }.resume()
+    }
+}
