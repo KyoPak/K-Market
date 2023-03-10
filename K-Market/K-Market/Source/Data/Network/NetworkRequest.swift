80d1a70a105f3fc6943ae9eca8505ff4109801e2
Kyo <hyosing92@gmail.com>
2023-03-10 16:22:34 +0900

    feat : NetworkRequest Protocol 수정

--- K-Market/K-Market/Source/Data/Network/NetworkRequest.swift
+++ K-Market/K-Market/Source/Data/Network/NetworkRequest.swift
@@ -8,28 +8,24 @@
 import Foundation
 
 protocol NetworkRequest {
-    var baseURL: String { get }
-    
-    var path: String { get }
-    var query: [URLQueryItem] { get }
+    var baseURL: String? { get }
+    var path: String? { get }
+    var query: [URLQueryItem]? { get }
     var httpMethod: HttpMethod { get }
+    var url: URL? { get }
 }
 
 extension NetworkRequest {
-    var baseURL: String {
+    var baseURL: String? {
         return "https://openmarket.yagom-academy.kr"
     }
     
-    func createURL() -> URL? {
-        var host = URLComponents(string: baseURL)
-        
-        host?.path = path
-        host?.queryItems = query
-        
-        return host?.url
+    var url : URL? {
+        return URLComponents.createURL(path: baseURL ?? "", queryItem: query)
     }
     
-    func createRequest(url: URL) -> URLRequest? {
+    func createRequest() -> URLRequest? {
+        guard let url = url else { return nil }
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
         
