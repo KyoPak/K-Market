27bfffd434dbd45686ce60056f01bbf2b278e5a3
Kyo <hyosing92@gmail.com>
2023-03-11 14:40:02 +0900

    feat : ListViewModel Protocol 정의 및 DefaultListViewModel 구현

--- K-Market/K-Market/Source/Present/MainScene/ViewModel/ListViewModel.swift
+++ K-Market/K-Market/Source/Present/MainScene/ViewModel/ListViewModel.swift
@@ -8,9 +8,38 @@
 import Foundation
 
 protocol ListViewModel {
-    
+    func bindDataList(completion: @escaping ([Product]) -> Void)
+    func fetchProductList()
 }
 
 final class DefaultListViewModel {
+    private var productList: [Product] = [] {
+        didSet {
+            dataListHandler?(productList)
+        }
+    }
     
+    private var dataListHandler: (([Product]) -> Void)?
+    
+    private let fetchUseCase: FetchProductDataUseCase
+    
+    init(fetchUseCase: FetchProductDataUseCase) {
+        self.fetchUseCase = fetchUseCase
+    }
+    
+    func bindDataList(completion: @escaping ([Product]) -> Void) {
+        dataListHandler = completion
+    }
+    
+    func fetchProductList(pageNo: Int, itemsPerPage: Int) {
+        fetchUseCase.fetchData(pageNo: pageNo, itemsPerPage: itemsPerPage) { [weak self] result in
+            switch result {
+            case .success(let datas):
+                self?.productList += datas
+            case .failure(let error):
+                //TODO: Delegate Alert
+                print(error)
+            }
+        }
+    }
 }
