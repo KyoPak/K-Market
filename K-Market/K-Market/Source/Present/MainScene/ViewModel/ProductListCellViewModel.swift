6854641d0de8410932a7290f0c7b88872f5ea23c
Kyo <hyosing92@gmail.com>
2023-03-11 19:45:34 +0900

    feat : ProductListCellViewModel Protocol 정의 및 DefaultProductListCellViewModel 구현

--- K-Market/K-Market/Source/Present/MainScene/ViewModel/ProductListCellViewModel.swift
+++ K-Market/K-Market/Source/Present/MainScene/ViewModel/ProductListCellViewModel.swift
@@ -8,22 +8,38 @@
 import Foundation
 
 protocol ProductListCellViewModel {
-    
+    func bindData(completion: @escaping (Product) -> Void)
+    func loadImage(completion: @escaping (Data?) -> Void)
 }
 
 final class DefaultProductListCellViewModel {
     private let product: Product
     private let locationData: LocationData?
+    private let loadImageUseCase: LoadImageUseCase
     
     init(
         product: Product,
         locationData: LocationData,
+        useCase: LoadImageUseCase
     ) {
         self.product = product
         self.locationData = locationData
+        self.loadImageUseCase = useCase
     }
     
     func bindData(completion: @escaping (Product) -> Void) {
         completion(product)
+    }
+    
+    func loadImage(completion: @escaping (Data?) -> Void) {
+        loadImageUseCase.loadImage(thumbnail: product.thumbnail) { result in
+            switch result {
+            case .success(let data):
+                completion(data)
+            case .failure(let error):
+                print("Error : " ,error)
+                completion(nil)
+            }
+        }
     }
 }
