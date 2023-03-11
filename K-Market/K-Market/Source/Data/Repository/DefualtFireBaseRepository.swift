3ab9320c4a38476ce798a9d2c3e84eae7f568074
Kyo <hyosing92@gmail.com>
2023-03-11 10:43:20 +0900

    refactor : DefualtFireBaseRepository 파일 별도로 분리

---
+++ K-Market/K-Market/Source/Data/Repository/DefualtFireBaseRepository.swift
@@ -0,0 +1,34 @@
+//
+//  DefualtFireBaseRepository.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/11.
+//
+
+import Foundation
+
+final class DefualtFireBaseRepository {
+    private let service: FireBaseService
+    
+    init(service: FireBaseService) {
+        self.service = service
+    }
+    
+    func load(completion: @escaping ([LocationData]) -> Void) {
+        service.load { datas in
+            completion(datas)
+        }
+    }
+    
+    func add(data: LocationData) {
+        service.add(data: data)
+    }
+    
+    func update(data: LocationData) {
+        service.update(data: data)
+    }
+    
+    func delete(data: LocationData) {
+        service.delete(id: data.id)
+    }
+}
--- K-Market/K-Market/Source/Domain/RepositoryInterface/FireBaseRepository.swift
+++ K-Market/K-Market/Source/Domain/RepositoryInterface/FireBaseRepository.swift
@@ -13,29 +13,3 @@ protocol FireBaseRepository {
     func update()
     func delete(data: LocationData)
 }
-
-final class DefualtFireBaseRepository {
-    private let service: FireBaseService
-    
-    init(service: FireBaseService) {
-        self.service = service
-    }
-    
-    func load(completion: @escaping ([LocationData]) -> Void) {
-        service.load { datas in
-            completion(datas)
-        }
-    }
-    
-    func add(data: LocationData) {
-        service.add(data: data)
-    }
-    
-    func update(data: LocationData) {
-        service.update(data: data)
-    }
-    
-    func delete(data: LocationData) {
-        service.delete(id: data.id)
-    }
-}
