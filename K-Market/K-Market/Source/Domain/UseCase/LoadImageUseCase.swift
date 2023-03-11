36fc11132bed00452a8afb1d23465b2fd9eaca03
Kyo <hyosing92@gmail.com>
2023-03-11 19:36:15 +0900

    feat : LoadImageUseCase Protocol 정의 및 DefaultLoadImageUseCase 구현

--- K-Market/K-Market/Source/Domain/UseCase/LoadImageUseCase.swift
+++ K-Market/K-Market/Source/Domain/UseCase/LoadImageUseCase.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol LoadImageUseCase {
+    func loadImage(thumbnail: String, completion: @escaping (Result<Data, NetworkError>) -> Void)
+}
+
+final class DefaultLoadImageUseCase: LoadImageUseCase {
+    private let productRepository: ProductRepository
+    
+    init(productRepository: ProductRepository) {
+        self.productRepository = productRepository
+    }
+    
+    func loadImage(thumbnail: String, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        let request = ImageLoadRequest(thumbnail: thumbnail)
+        
+        productRepository.request(customRequest: request) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
