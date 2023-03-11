56f46d5ffb9994ab26caa0e0b45e1bb8528d5a91
Kyo <hyosing92@gmail.com>
2023-03-11 12:19:02 +0900

    feat : FetchProductDataUseCase Protocol 정의 및 DefaultFetchProductDataUseCase 구현

---
+++ K-Market/K-Market/Source/Domain/UseCase/FetchProductDataUseCase.swift
@@ -0,0 +1,58 @@
+//
+//  FetchProductDataUseCase.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/11.
+//
+
+import Foundation
+
+protocol FetchProductDataUseCase {
+    func fetchData(
+        pageNo: Int,
+        itemsPerPage: Int,
+        completion: @escaping (Result<[Product], NetworkError>) -> Void
+    )
+}
+
+final class DefaultFetchProductDataUseCase {
+    private let networkRepository: NetworkRepository
+    
+    init(networkRepository: NetworkRepository) {
+        self.networkRepository = networkRepository
+    }
+    
+    func fetchData(
+        pageNo: Int,
+        itemsPerPage: Int,
+        completion: @escaping (Result<[Product], NetworkError>) -> Void
+    ) {
+        let request = ListFetchRequest(pageNo: pageNo, itemsPerPage: itemsPerPage)
+        networkRepository.request(customRequest: request) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let products = try self.convert(data: data)
+                    completion(.success(products))
+                } catch {
+                    guard let error = error as? NetworkError else { return }
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func convert(data: Data) throws -> [Product] {
+        let decodeManager = DecodeManager<ProductPage>()
+        let products = decodeManager.decode(data)
+        
+        switch products {
+        case .success(let productList):
+            return productList.pages
+        case .failure(let error):
+            throw error
+        }
+    }
+}
