68e3af49e20649dac17f2dc17b908a52df3bf3ad
Kyo <hyosing92@gmail.com>
2023-03-14 22:56:50 +0900

    fix : URLComponent Extension 메서드 path nil일 경우 return 값 수정

--- K-Market/K-Market/Source/Util/Extension/URLComponents+Extension.swift
+++ K-Market/K-Market/Source/Util/Extension/URLComponents+Extension.swift
@@ -9,8 +9,10 @@ import Foundation
 
 extension URLComponents {
     static func createURL(baseURL:String?, path: String?, queryItem: [URLQueryItem]?) -> URL? {
-        guard let base = baseURL, let path = path else { return nil }
+        guard let base = baseURL else { return nil }
         var host = URLComponents(string: base)
+        
+        guard let path = path else { return host?.url }
         
         host?.path = path
         host?.queryItems = queryItem
