1e887e64c5c7b013957f23330e29d9fb9d922a45
Kyo <hyosing92@gmail.com>
2023-03-17 12:46:02 +0900

    configure : DetailView 구현 전 ViewController, ViewModel 파일, 폴더 생성

---
+++ K-Market/K-Market/Source/Present/DetailScene/View/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/17.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ K-Market/K-Market/Source/Present/DetailScene/ViewModel/DetailViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewModel.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/17.
+//
+
+import Foundation
+
+protocol DetailViewModelInput {
+    
+}
+
+protocol DetailViewModelOutput {
+    
+}
+
+protocol DetailViewModel: DetailViewModelInput, DetailViewModelOutput { }
+
+final class DefaultDetailViewModel: DetailViewModel {
+    
+}
