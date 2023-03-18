ab7bffb9dfc0ae7548b586b0532fe55afa1250a2
Kyo <hyosing92@gmail.com>
2023-03-18 16:27:02 +0900

    feat : AddViewModel 정의 및 DefaultAddViewModel 구현

--- K-Market/K-Market/Source/Present/AddScene/ViewModel/AddViewModel.swift
+++ K-Market/K-Market/Source/Present/AddScene/ViewModel/AddViewModel.swift
@@ -7,6 +7,54 @@
 
 import Foundation
 
-protocol AddViewModel {
+protocol AddViewModelInput {
+    func addImageData(_ data: Data)
+    func setupProduct(_ product: Product)
+    func postProduct(completion: @escaping (Bool) -> Void)
+}
+
+protocol AddViewModelOutput { }
+
+protocol AddViewModel: AddViewModelInput, AddViewModelOutput { }
+
+final class DefualtAddViewModel: AddViewModel {
+    private var imageData: [Data] = []
+    private var product: Product?
     
+    private let fetchProductDetailUseCase: FetchProductDetailUseCase
+    private let postProductUseCase: PostProductUseCase
+    private let loadImageUseCase: LoadImageUseCase
+    
+    init(
+        fetchProductDetailUseCase: FetchProductDetailUseCase,
+        postProductUseCase: PostProductUseCase,
+        loadImageUseCase: LoadImageUseCase
+    ) {
+        self.fetchProductDetailUseCase = fetchProductDetailUseCase
+        self.postProductUseCase = postProductUseCase
+        self.loadImageUseCase = loadImageUseCase
+    }
+    
+    func addImageData(_ data: Data) {
+        imageData.append(data)
+    }
+    
+    func setupProduct(_ product: Product) {
+        self.product = product
+    }
+    
+    func postProduct(completion: @escaping (Bool) -> Void) {
+        guard let product = product else { return }
+        
+        postProductUseCase.postData(productData: product, imageDatas: imageData) { result in
+            switch result {
+            case .success(let check):
+                if !check {
+                    print(NetworkError.data)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
