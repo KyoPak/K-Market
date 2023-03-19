4ac9d0a07661c944059bded805c5d2d027a503ce
Kyo <hyosing92@gmail.com>
2023-03-20 00:51:53 +0900

    feat : DeleteURIRequest 구현

---
+++ K-Market/K-Market/Source/Data/ProductStorage/Infra/Request/DeleteURIRequest.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteURIRequest.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/20.
+//
+
+import Foundation
+
+struct DeleteURIRequest: CustomRequest {
+    var path: String?
+    var query: [URLQueryItem]?
+    var httpMethod: HTTPMethod = .POST
+    
+    init(id: Int) {
+        path = "/api/products/\(id)/archived"
+    }
+    
+    func createRequest() -> URLRequest? {
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        request = .setupIdentifier(request: &request)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let bodyData = try? JSONSerialization.data(withJSONObject: ["secret": "dk9r294wvfwkgvhn"])
+        request.httpBody = bodyData
+        
+        return request
+    }
+}
