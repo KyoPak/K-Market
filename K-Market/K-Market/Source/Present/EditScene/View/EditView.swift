ee99a314e105228e18878dfc733fa475fcc95e33
Kyo <hyosing92@gmail.com>
2023-03-19 23:04:50 +0900

    feat : EditView UploadView 상속후 구현

--- K-Market/K-Market/Source/Present/EditScene/View/EditView.swift
+++ K-Market/K-Market/Source/Present/EditScene/View/EditView.swift
@@ -7,6 +7,39 @@
 
 import UIKit
 
-class EditView: UIView {
+final class EditView: UploadView {
+    private let viewModel: EditViewModel
     
+    init(viewModel: EditViewModel) {
+        self.viewModel = viewModel
+        super.init()
+        setupLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Method
+extension EditView {
+    private func setupLabel() {
+        nameTextField.text = viewModel.product.name
+        descriptionTextView.text = viewModel.product.description
+        stockTextField.text = String(viewModel.product.stock)
+        priceTextField.text = String(viewModel.product.price)
+        salePriceTextField.text = String(viewModel.product.bargainPrice)
+        currencySegmentedControl.selectedSegmentIndex = viewModel.product.currency == .KRW ? .zero : 1
+    }
+    
+    func packageData() {
+        viewModel.setupProduct(
+            name: nameTextField.text,
+            price: priceTextField.text,
+            salePrice: salePriceTextField.text,
+            stock: stockTextField.text,
+            description: descriptionTextView.text,
+            currencyIndex: currencySegmentedControl.selectedSegmentIndex
+        )
+    }
 }
