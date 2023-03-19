c5420727be05c6311290d105fa02f12d6e356d2c
Kyo <hyosing92@gmail.com>
2023-03-19 11:18:43 +0900

    feat : Request 관련 Extension들 폴더 분리

---
+++ K-Market/K-Market/Source/Data/ProductStorage/Infra/Util/Extension/URLRequest+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  URLRequest+Extension.swift
+//  K-Market
+//
+//  Created by parkhyo on 2023/03/19.
+//
+
+import Foundation
+
+extension URLRequest {
+    static func setupIdentifier(request: inout URLRequest) -> URLRequest {
+        request.setValue(
+            "0574c520-6942-11ed-a917-43299f97bee6",
+            forHTTPHeaderField: "identifier"
+        )
+        return request
+    }
+}
