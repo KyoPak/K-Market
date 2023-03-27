b99ec361ce2be5ef44465cc65d6a55bda0c38cf8
Kyo <hyosing92@gmail.com>
2023-03-27 15:59:46 +0900

    test : DetailViewModel Test에 test_setup추가

--- K-Market/K-MarketTests/Present/DetailViewModelTest.swift
+++ K-Market/K-MarketTests/Present/DetailViewModelTest.swift
@@ -51,8 +51,8 @@ final class DetailViewModelTest: XCTestCase {
         
         let expectation = XCTestExpectation(description: "정상적인 변경")
         
-        // When
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // When
             let text = self.detailViewModel.customStockText()
             if "수량 : 1K" ==  text {
                 // Then
@@ -69,8 +69,8 @@ final class DetailViewModelTest: XCTestCase {
         
         let expectation = XCTestExpectation(description: "정상적인 변경")
         
-        // When
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // When
             let text = self.detailViewModel.customPriceText(self.detailViewModel.product.value?.price)
             if "KRW 1.0K" ==  text {
                 // Then
@@ -79,5 +79,30 @@ final class DetailViewModelTest: XCTestCase {
         }
         
         wait(for: [expectation], timeout: 3.0)
+    }
+    
+    func test_setup() {
+        // Given
+        let productID = 10
+        let productName = "test10"
+        let productThumbnail = "test10"
+        let productPrice: Double = 1000
+        
+        let expectation = XCTestExpectation(description: "동일한 데이터")
+        
+        // When
+        detailViewModel.setup()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            let data = self.detailViewModel.product.value
+            // Then
+            XCTAssertEqual(productID, data?.id)
+            XCTAssertEqual(productName, data?.name)
+            XCTAssertEqual(productThumbnail, data?.thumbnail)
+            XCTAssertEqual(productPrice, data?.price)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
     }
 }
